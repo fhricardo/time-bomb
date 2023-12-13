a6f1a6d0f4ca76fb5ea4332f55be6e1ee8882c4e
Flavio Ricardo <fhricardo@gmail.com>
2023-12-13 15:20:16 -0300

    Ajustes de Script; Fios Aleatórios; Adição de Funcionalidades e Mensagens

--- js/countdown.js
+++ js/countdown.js
@@ -1,59 +1,111 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            timer = duration;
-        }
-        // Pisca Led Verde
-        if (timer % 2 == 0) {
-            gLed = "assets/led-green-on.png"
-            document.getElementById("greenLed").src = gLed
-        } else {
-            gLed = "assets/led-green-off.png"
-            document.getElementById("greenLed").src = gLed
-        }
-        // Pisca Led Vermelho
-        if (timer % 2 == 1) {
-            rLed = "assets/led-red-on.png"
-            document.getElementById("redLed").src = rLed
-        } else {
-            rLed = "assets/led-red-off.png"
-            document.getElementById("redLed").src = rLed
-        }
-    }, 1000);
-}
-window.onload = function () {
-    var duration = 60 * 5; // Converter para segundos
-    display = document.querySelector('#timer'); // selecionando o timer
-    startTimer(duration, display); // iniciando o timer
-};
+const theTimer = document.getElementById('timer');
+const rwl = document.getElementById("rwl");
+const bwr = document.getElementById("bwr");
+const rwr = document.getElementById("rwr");
+const bwl = document.getElementById("bwl");
+const redLed = document.getElementById("redLed");
+const greenLed = document.getElementById("greenLed");
+const bomb = document.getElementById('bomb');
+const explosion = document.getElementById('explosion');
+const message = document.getElementById('message');
+window.onload = function addEvents() {
+    var maximo = 4;
+    var i, arr = [];
+    for (i = 0; i < maximo; i++) {
+        arr[i] = i + 1;
+    }
+    var p, n, tmp;
+    for (p = arr.length; p;) {
+        n = Math.random() * p-- | 0;
+        tmp = arr[n];
+        arr[n] = arr[p];
+        arr[p] = tmp;
+    }
 
-var rwl = document.getElementById("rwl")
-rwl.addEventListener('click', cutRWL)
-function cutRWL() {
-    rwlCut = "assets/red-wire-left-cut.png"
-    document.getElementById("rwl").src = rwlCut
+    rwl.setAttribute("onclick", "cutWire(" + arr[0] + ",'rwl')");
+    bwl.setAttribute("onclick", "cutWire(" + arr[1] + ",'bwl')");
+    rwr.setAttribute("onclick", "cutWire(" + arr[2] + ",'rwr')");
+    bwr.setAttribute("onclick", "cutWire(" + arr[3] + ",'bwr')");
 }
-var bwl = document.getElementById("bwl")
-bwl.addEventListener('click', cutBWL)
-function cutBWL() {
-    bwlCut = "assets/black-wire-left-cut.png"
-    document.getElementById("bwl").src = bwlCut
+
+function cutWire(on, div) {
+    switch (div) {
+        case 'rwl':
+            console.log(div)
+            rwlCut = "assets/red-wire-left-cut.png";
+            rwl.src = rwlCut;
+            rwl.setAttribute('onclick', "cutRWL(0)");
+            break;
+        case 'bwl':
+            console.log(div)
+            bwlCut = "assets/black-wire-left-cut.png"
+            bwl.src = bwlCut
+            bwl.setAttribute('onclick', "cutBWL(0)")
+            break;
+        case 'rwr':
+            console.log(div)
+            rwrCut = "assets/red-wire-right-cut.png"
+            rwr.src = rwrCut
+            rwr.setAttribute('onclick', "cutRWR(0)")
+            break;
+        case 'bwr':
+            console.log(div)
+            bwrCut = "assets/black-wire-right-cut.png"
+            bwr.src = bwrCut
+            bwr.setAttribute('onclick', "cutBWR(0)")
+            break;
+    }
+    switch (on) {
+        case 1:
+            bomb.setAttribute('class', 'hide');
+            redLed.setAttribute('class', 'hide');
+            greenLed.setAttribute('class', 'hide');
+            message.setAttribute('class', 'hide');
+            explosion.setAttribute('class', 'showExplosion');
+            break;
+        case 2:
+            time = 300;
+            message.innerHTML = 'você ganhou mais tempo...'
+            break;
+        case 3:
+            speed = 500;
+            message.innerHTML = 'O timer acelerou, é melhor correr!'
+            break;
+        case 4:
+            message.innerHTML = 'Você salvou o dia!'
+            break;
+    }
 }
-var rwr = document.getElementById("rwr")
-rwr.addEventListener('click', cutRWR)
-function cutRWR() {
-    rwrCut = "assets/red-wire-right-cut.png"
-    document.getElementById("rwr").src = rwrCut
+
+var time = 300;
+var speed = 1000;
+function countdown(value) {
+    if ((time - 1) >= -1) {
+        var minute = parseInt(time / 60);
+        var second = time % 60;
+        if (minute < 10) {
+            minute = "0" + minute;
+        }
+        if (second < 10) {
+            second = "0" + second;
+        }
+        if (second % 2 == 0) {
+            redLed.src = "assets/led-red-on.png"
+            greenLed.src = "assets/led-green-off.png"
+        } else {
+            redLed.src = "assets/led-red-off.png"
+            greenLed.src = "assets/led-green-on.png"
+        }
+        timer.innerHTML = minute + ":" + second;
+        if (minute == 0 && second == 0) {
+            bomb.setAttribute('class', 'hide');
+            redLed.setAttribute('class', 'hide');
+            greenLed.setAttribute('class', 'hide');
+            explosion.setAttribute('class', 'showExplosion');
+        }
+        console.log(minute + ":" + second);
+        setTimeout('countdown()', speed);
+        time--;
+    }
 }
-var bwr = document.getElementById("bwr")
-bwr.addEventListener('click', cutBWR)
-function cutBWR() {
-    bwrCut = "assets/black-wire-right-cut.png"
-    document.getElementById("bwr").src = bwrCut
-}
+countdown();
